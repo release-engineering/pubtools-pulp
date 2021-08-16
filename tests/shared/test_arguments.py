3460b06caf00c96b309ce5378f4be9fe95404708
Ryan Smith <avlwx2014@gmail.com>
2021-08-16 12:25:04 -0400

    Update test cases to make use of paramtrization rather than loops

--- tests/shared/test_arguments.py
+++ tests/shared/test_arguments.py
@@ -13,19 +13,21 @@ def parser():
     return ArgumentParser()
 
 
-def test_split_and_extend_single_delimited_instance():
+@pytest.fixture(params=[",", ".", "-", "/"])
+def delimiter(request):
+    """Provide parameterization for testing different delimiters."""
+    return request.param
+
+
+def test_split_and_extend_single_delimited_instance(parser, delimiter):
     """Test a single option instance, with all values in a delimited list."""
     # test one instance, all delimited using different delimiters
-    for delimiter in ",.-/":
-        parser = ArgumentParser()
-        parser.add_argument(
-            "--option", type=str, action=SplitAndExtend, split_on=delimiter
-        )
+    parser.add_argument("--option", type=str, action=SplitAndExtend, split_on=delimiter)
 
-        sys.argv = ["fake-command", "--option", delimiter.join(FAKE_OPTION_VALUES)]
+    sys.argv = ["command", "--option", delimiter.join(FAKE_OPTION_VALUES)]
 
-        args = parser.parse_args()
-        assert args.option == FAKE_OPTION_VALUES
+    args = parser.parse_args()
+    assert args.option == FAKE_OPTION_VALUES
 
 
 def test_split_and_extend_multiple_option_instances(parser):
@@ -41,36 +43,43 @@ def test_split_and_extend_multiple_option_instances(parser):
     assert args.option == FAKE_OPTION_VALUES
 
 
-def test_split_and_extend_multiple_mix_and_match_instance():
+def test_split_and_extend_multiple_mix_and_match_instance(parser, delimiter):
     """Test multiple option instances, some with delimited lists."""
     # test mix-and-match, delimited-and-not, using different delimiters
-    for delimiter in ",.-/":
-        parser = ArgumentParser()
-        parser.add_argument(
-            "--option", type=str, action=SplitAndExtend, split_on=delimiter
-        )
+    parser.add_argument("--option", type=str, action=SplitAndExtend, split_on=delimiter)
 
-        sys.argv = [
-            "fake-command",
-            "--option",
-            delimiter.join(FAKE_OPTION_VALUES[:-1]),
-            "--option",
-            FAKE_OPTION_VALUES[-1],
-        ]
+    sys.argv = [
+        "command",
+        "--option",
+        delimiter.join(FAKE_OPTION_VALUES[:-1]),
+        "--option",
+        FAKE_OPTION_VALUES[-1],
+    ]
 
-        args = parser.parse_args()
-        assert args.option == FAKE_OPTION_VALUES
+    args = parser.parse_args()
+    assert args.option == FAKE_OPTION_VALUES
 
 
 def test_split_and_extend_multiple_instances_with_trailing_delimiters(parser):
     """Test multiple instances, each with an extra trailing delimiter."""
+
     parser.add_argument("--option", type=str, action=SplitAndExtend)
 
     # test multiple, individual instances with extra
-    # trailing delimiters e.g. --option value1, --option value2,
-    sys.argv = ["command"]
-    for value in FAKE_OPTION_VALUES:
-        sys.argv.extend(["--option", "{},".format(value)])
+    # delimiters e.g. `--option value1,` or `--option value1,,value2`
+    sys.argv = [
+        "command",
+        "--option",
+        "value0,",
+        "--option",
+        ",value1,value2,,",
+        "--option",
+        "value3,,value4",
+        "--option",
+        ",,,value5",
+    ]
+
+    expected = ["value{}".format(i) for i in range(6)]
 
     args = parser.parse_args()
-    assert args.option == FAKE_OPTION_VALUES
+    assert args.option == expected
