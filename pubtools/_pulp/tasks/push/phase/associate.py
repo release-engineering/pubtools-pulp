93d504d2ec55d847d104c953fc13e0ca1a6ddced
Rohan McGovern <rmcgover@redhat.com>
2022-03-04 10:01:12 +1000

    push: reduce modulemd-related blocking in associate
    We want to ensure that RPMs are always associated into target repos only
    after any corresponding modulemds. Previously, the simplistic solution
    to this was simply to not associate *anything* until *all* uploads had
    completed, but that causes unnecessary blocking in some cases and
    potentially wastes a lot of time for pushes which don't even use RPMs or
    modules.
    
    With a minor refactor we can do a little better: make associate process
    RPMs last, only after all other items have made it into the phase. Fixes
    a TODO.
    
    This could still be improved further by tracking information on which
    modules/RPMs correspond to each other and in which repos, but the
    performance vs complexity trade-off isn't clear right now.

--- pubtools/_pulp/tasks/push/phase/associate.py
+++ pubtools/_pulp/tasks/push/phase/associate.py
@@ -1,5 +1,5 @@
 from .base import Phase, BATCH_SIZE
-from ..items import PulpPushItem
+from ..items import PulpPushItem, PulpRpmPushItem
 
 
 class Associate(Phase):
@@ -22,23 +22,52 @@ class Associate(Phase):
         self.pulp_client = pulp_client
         self.pre_push = pre_push
 
+    def iter_for_associate(self):
+        """A special batched iterator for this phase which reorders all RPMs
+        to the end of the queue.
+
+        Why: it is strongly encouraged to ensure all modulemds are put into repos
+        before we start putting RPMs into them, to reduce the risk that we could
+        accidentally expose an RPM without the corresponding modulemd as that
+        can break systems consuming the repos. Therefore we need to ensure any
+        module items are processed before we can proceed to associate any RPM
+        items.
+
+        Note this potentially could be made smarter by, for example, tracking
+        the state of modulemds for each repo, or even fully tracking the
+        dependencies between each modulemd and the RPMs referenced by it, but
+        it's unclear whether the additional complexity would be worth it.
+        """
+
+        yield_later = []
+
+        for batch in self.iter_input_batched():
+            yield_now = []
+
+            for item in batch:
+                if isinstance(item, PulpRpmPushItem):
+                    # RPMs are held until later
+                    yield_later.append(item)
+                else:
+                    yield_now.append(item)
+
+            if yield_now:
+                yield yield_now
+
+        # OK, everything other than RPMs have been seen already.
+        # By this point we know that modulemds are all in the right repos (noting
+        # that modulemds are fully handled during upload phase, so by the time
+        # we see a modulemd item in this phase, it's all done).
+        #
+        # That means it's safe to go ahead and yield RPMs, since any corresponding
+        # modulemds must be in place.
+        while yield_later:
+            batch = yield_later[:BATCH_SIZE]
+            yield_later = yield_later[BATCH_SIZE:]
+            yield batch
+
     def run(self):
-        # Synchronization point prior to association.
-        #
-        # Why: it is strongly encouraged to ensure all modulemds are put into repos
-        # before we start putting RPMs into them, to reduce the risk that we could
-        # accidentally expose an RPM without the corresponding modulemd. Therefore we
-        # need to ensure any module items are processed by uploaded_items above,
-        # before we can proceed to associate any RPM items.
-        #
-        # TODO: try to make this smarter so it handles only that modulemd/rpm case
-        # described above without slowing other stuff down?
-        remaining = list(self.iter_input())
-
-        while remaining:
-            batch = remaining[:BATCH_SIZE]
-            remaining = remaining[BATCH_SIZE:]
-
+        for batch in self.iter_for_associate():
             for items in PulpPushItem.items_by_type(batch):
                 for associated_f in PulpPushItem.associated_items_single_batch(
                     self.pulp_client, items
